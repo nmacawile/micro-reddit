bf23568324342d9bf6c43945227fd40006934198
nmacawile <nmacawile@gmail.com>
2017-05-19 01:55:06 +0800

    Add tests for Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = User.new(name: "test", email: "test@test.com")
+		@user.save
+		@post = Post.new(body: "text here", author: @user)		
+	end
+
+	test "should be valid" do
+		assert @post.valid?
+	end
+
+	test "should keep reference to the author" do
+		@post.save
+		assert @post.author == @user
+	end
+
+	test "should keep references to all comments made under it" do
+		@post.save
+		comment1 = Comment.new(body: "comment1", post: @post, user: @user)
+		comment1.save
+		comment2 = Comment.new(body: "comment2", post: @post, user: @user)
+		comment2.save
+		assert @post.comments == [comment1, comment2]
+	end
+
 end
